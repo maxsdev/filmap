8165c757524e23fc68d8c631a88b95b3e20aafaf
maxsdev <maxsdev@yandex.ru>
2017-03-10 00:10:55 +0700

    front-end

--- www/api/index.php
+++ www/api/index.php
@@ -2,9 +2,13 @@
 
 if (count($_GET) > 0) {
   require_once 'MarkerProvider.php';
-
-  $mp = new MarkerProvider($_GET['longitude'], $_GET['latitude']);
-  echo $mp->getMarkers();
+  $lng = $_GET['longitude'];
+  $lat = $_GET['latitude'];
+  $zoom = $_GET['zoom'];
+  $mp = new MarkerProvider($lng, $lat);
+  $json_obj = array('longitude'=>$lng, 'latitude'=>$lat, 'zoom'=>$zoom);
+  echo json_encode($json_obj);
+  // echo $mp->getMarkers();
 }
 
 ?>
